64c2d3009c10cd7fea48c690aa79270316a356ad
zendagin <zendagin1126@gmail.com>
2022-11-11 17:55:41 +0900

    feat: init home and layout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
-})
+    app: {
+        head: {
+            title: "Portfolio | Cheng Tak Kin",
+        }
+    },
+    runtimeConfig: {
+        secret: "private"
+    },
+    appConfig: {
+        config: "public"
+    }
+});
