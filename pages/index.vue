92f155a3e21653ad2b06c3c59bff45dfb2c055b0
zendagin <zendagin1126@gmail.com>
2022-11-15 02:51:29 +0900

    feat: sample text and style

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,18 @@
             <h1>About me</h1>
         </div>
         <div class="content">
+            <section>
+                <h2 class="subtitle">Who am I</h2>
+                <p>
+                    I am Cheng Tak Kin, a software developer from Hong Kong.
+                </p>
+            </section>
+            <section>
+                <h2 class="subtitle">What I do</h2>
+                <p>
+                    I am Cheng Tak Kin, a software developer from Hong Kong.
+                </p>
+            </section>
         </div>
     </div>
 </template>
@@ -11,8 +23,12 @@
 <script setup lang="ts">
 </script>
 
-<style>
+<style scoped>
 .main {
     padding-bottom: 100px;
+}
+
+section:not(:last-child) {
+    @apply mb-8;
 }
 </style>
--- pages/works.vue
+++ pages/works.vue
@@ -11,7 +11,7 @@
 <script setup lang="ts">
 </script>
 
-<style>
+<style scoped>
 .main {
     padding-bottom: 100px;
 }
