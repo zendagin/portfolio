b31a4cd82e83c0cc6038ba44818ca14cb58189dc
zendagin <zendagin1126@gmail.com>
2022-11-15 03:45:10 +0900

    feat: works

--- pages/works.vue
+++ pages/works.vue
@@ -3,8 +3,52 @@
         <div class="title">
             <h1>My works</h1>
         </div>
-        <div class="content">
-        </div>
+        <section class="content">
+            <div class="flex justify-between">
+                <h2 class="subtitle">AfterSchool</h2>
+                <a href="https://afterschool.com.hk" target="_blank">
+                    <button class="btn btn-blue">access</button>
+                </a>
+            </div>
+
+            <div>
+                <div class="py-6">
+                    <img src="~/assets/img/afterschool-website.png" />
+                </div>
+                <div>
+                    <p>
+                        A large scale e-learning platform built with Nuxt2. Worked with profession designer through
+                        Figma. Using
+                        Vuex to achieved complicated state management. Setup data like metadata, sitemap and structured
+                        data, for SEO purpose.
+                    </p>
+                    <p>
+                        The frontend is developed by 3 developers including me.
+                    </p>
+                </div>
+            </div>
+        </section>
+        <section class="content">
+            <div class="flex justify-between">
+                <h2 class="subtitle">Shoperuse</h2>
+                <a href="https://shoperuse.com" target="_blank">
+                    <button class="btn btn-blue">access</button>
+                </a>
+            </div>
+
+            <div>
+                <div class="py-6">
+                    <img src="~/assets/img/shoperuse-website.png" />
+                </div>
+                <div>
+                    <p>
+                        A side project worked with another partner. It is an instagram shop catalog of Hong Kong. We
+                        utilize the opportunity to try new technologies like Nuxt3, windicss and deploying on Google App
+                        Engine.
+                    </p>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -14,5 +58,9 @@
 <style scoped>
 .main {
     padding-bottom: 100px;
+}
+
+section:not(:last-child) {
+    @apply mb-10;
 }
 </style>
