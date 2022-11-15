e6addbe8b6ead804b885c413962a08b0d143408b
zendagin <zendagin1126@gmail.com>
2022-11-15 17:14:09 +0900

    feat: update works description

--- pages/works.vue
+++ pages/works.vue
@@ -5,21 +5,31 @@
         </div>
         <WorkCard class="card" name="AfterSchool" url="https://afterschool.com.hk" img="/img/afterschool-website.png">
             <p>
-                A large scale e-learning platform built with Nuxt2. Worked with profession designer through
-                Figma. Using
-                Vuex to achieved complicated state management. Setup data like metadata, sitemap and structured
-                data, for SEO purpose.
+                A large scale e-learning platform built with Nuxt2. Developed by 3 developers including me.
             </p>
-            <p>
-                The frontend is developed by 3 developers including me.
-            </p>
+            <div class="pl-6">
+                <ul class="list-disc">
+                    <li>Using <b>Figma</b> to cooperate with designer</li>
+                    <li>Using <b>Bootstrap, breakpoints and media queries</b> to achieved responsiveness</li>
+                    <li>Using <b>Vuex</b> to achieve complicated state management</li>
+                    <li>Seting data like metadata, sitemap and structured
+                        data for SEO purpose</li>
+                    <li>Deploying on <b>AWS</b>.</li>
+                </ul>
+            </div>
         </WorkCard>
         <WorkCard class="card" name="Shoperuse" url="https://shoperuse.com" img="/img/shoperuse-website.png">
             <p>
                 A side project worked with another partner. It is an instagram shop catalog of Hong Kong. We
-                utilize the opportunity to try new technologies like Nuxt3, windicss and deploying on Google App
-                Engine.
+                utilize the opportunity to try new technologies.
             </p>
+            <div class="pl-6">
+                <ul class="list-disc">
+                    <li>Using <b>Windicss</b> which based on <b>Tailwind CSS</b>.</li>
+                    <li>Using <b>Nuxt3</b>, with Vue 3 Composition API.</li>
+                    <li>Deploying on <b>GCP</b>.</li>
+                </ul>
+            </div>
         </WorkCard>
     </div>
 </template>
